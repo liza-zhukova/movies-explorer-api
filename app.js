075714afde220acd700360520990db5bde1ca23b
Liza Zhukova <Elishkaaaa@yandex.ru>
2023-06-03 18:51:49 +0300

    fix: change name of error

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const cookieParser = require('cookie-parser');
 const {
   celebrate, Joi, errors,
 } = require('celebrate');
+const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
+const { validateUrl } = require('./utils/validateUrl');
 const NotFoundError = require('./errors/notFoundError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
@@ -20,7 +22,35 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.post(
+  '/signin',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+    }),
+  }),
+  login,
+);
+
+app.post(
+  '/signup',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string().custom(validateUrl),
+    }),
+  }),
+  createUser,
+);
+
 app.use(auth);
+
+app.use(require('./routes/users'));
+app.use(require('./routes/movies'));
 
 app.use('*', (req, res, next) => {
   next(new NotFoundError('Страница не найдена'));
